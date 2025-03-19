65f27f40260572604e164092f3b9e57518a79327
Samir Satam <ssatam@gmail.com>
2025-03-19 15:53:35 -0400

    Added a StreamLit frontend running on port localhost:8501

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # LangChain Documentation Helper
+
+Make sure to download the documentation, and unzip it in the documentation-helper/langchain-docs folder.
+
+StreamLit lightweight app to query the LLM for LangChain Documentation.
--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from documentation-helper!")
+from typing import Set
+
+import streamlit as st
+
+from backend.core import run_llm
+
+st.header("LanChain Udemy course documentation helper bot.")
+
+prompt  = st.text_input("Prompt", placeholder="Enter your prompt here.")
+
+if "user_prompt_history" not in st.session_state:
+    st.session_state["user_prompt_history"] = []
+
+if "chat_answers_history" not in st.session_state:
+    st.session_state["chat_answers_history"] = []
+
+def create_sources_string(source_urls: Set[str]) -> str:
+    if not source_urls:
+        return ""
+    sources_list = sorted(list(source_urls))
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list):
+        sources_string += f"\n{i+1} - {source}\n"
+    return sources_string
 
 
-if __name__ == "__main__":
-    main()
+if prompt:
+    with st.spinner("Generating response"):
+        generated_response = run_llm(query=prompt)
+        print(generated_response)
+        sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])
+        formatted_response = (
+            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+        )
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answers_history"].append(formatted_response)
+
+if st.session_state["chat_answers_history"]:
+    for generated_response, user_query in zip(st.session_state["user_prompt_history"], st.session_state["chat_answers_history"]):
+        st.chat_message("user").write(user_query)
+        st.chat_message("assistant").write(generated_response)
