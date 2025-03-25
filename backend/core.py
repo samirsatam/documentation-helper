4a66a98c568d254891bcfbab88005314e35e97c3
Samir Satam <ssatam@gmail.com>
2025-03-25 16:14:51 -0400

    Added chat history.

--- backend/core.py
+++ backend/core.py
@@ -1,6 +1,9 @@
+from typing import List, Dict, Any
+
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
@@ -11,17 +14,27 @@ load_dotenv()
 
 INDEX_NAME ="langchain-doc-index"
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     text_embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=text_embeddings)
     chat = ChatOpenAI(verbose=True, temperature=0)
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
-    qa = create_retrieval_chain(
+
+    # using memory with RAG, 1. The 'rephrase' prompt supplies the 'chat_history'
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat,
         retriever=docsearch.as_retriever(),
+        prompt=rephrase_prompt
+    )
+
+    qa = create_retrieval_chain(
+        retriever=history_aware_retriever, # 2. chaining in the history_aware_retriever
         combine_docs_chain=stuff_documents_chain
     )
-    result = qa.invoke(input={"input":query})
+    #3. pass in the chat history
+    result = qa.invoke(input={"input":query, "chat_history":chat_history})
     new_result = {
         "query": result["input"],
         "result": result["answer"],
--- main.py
+++ main.py
@@ -14,6 +14,18 @@ if "user_prompt_history" not in st.session_state:
 if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
 
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+if (
+    "chat_answers_history" not in st.session_state
+    and "user_prompt_history" not in st.session_state
+    and "chat_history" not in st.session_state
+):
+    st.session_state["chat_answers_history"] = []
+    st.session_state["user_prompt_history"] = []
+    st.session_state["chat_history"] = []
+
 def create_sources_string(source_urls: Set[str]) -> str:
     if not source_urls:
         return ""
@@ -26,7 +38,10 @@ def create_sources_string(source_urls: Set[str]) -> str:
 
 if prompt:
     with st.spinner("Generating response"):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(
+            query=prompt,
+            chat_history=st.session_state["chat_history"]
+        )
         print(generated_response)
         sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])
         formatted_response = (
@@ -34,6 +49,10 @@ if prompt:
         )
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
+        # Adding memory to RAG.
+        # note this tuple format, ("human", ...) and ("ai", ....) is something that LangChain understands.
+        st.session_state["chat_history"].append(("human", prompt))
+        st.session_state["chat_history"].append(("ai", generated_response["result"]))
 
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(st.session_state["user_prompt_history"], st.session_state["chat_answers_history"]):
